4df18588ffc011f3427b7fda3802f2fff4237c5e
Alijalal-Dev <aj0546006@gmail.com>
2024-08-29 05:52:26 -0700

    uplaod everyting

--- cmd_helper.c
+++ cmd_helper.c
@@ -92,7 +92,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 		buf[k++] = '\0', info->cmd_buf_type = OR_CMD;
 	else if (buf[k] == '&' && buf[k + 1] == '&')
 		buf[k++] = '\0', info->cmd_buf_type = AND_CMD;
-	else if (buf[j] == ';')
+	else if (buf[k] == ';')
 		buf[k] = '\0', info->cmd_buf_type = CHAIN_CMD;
 	else
 		return (0);
